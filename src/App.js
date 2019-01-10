ceb787981f5697aaac16e8926e64e44d0ff6313a
Andrew <andrewplewak@gmail.com>
2019-01-09 21:04:26 -0500

    update

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
       super();
       
       this.state = {
-        headerValue : '{APLEWAK@poop.IO}'
+        headerValue : '{APLEWAK@GITHUB.IO}'
         , softwareTextisOpen : false
         , hardwareTextisOpen : false
         , contactTextisOpen : false
