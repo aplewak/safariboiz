0200c3eae5027533fe44ac540f621ca75d1b0734
Andrew <andrewplewak@gmail.com>
2019-03-01 17:41:49 -0500

    new code for boost

--- src/GetDetailTextForBoost.jsx
+++ src/GetDetailTextForBoost.jsx
@@ -8,7 +8,8 @@ import man from '../src/IMAGES/manif.JPG'
 import map from '../src/IMAGES/map.JPG'
 import pwm from '../src/IMAGES/pwm.JPG'
 import twg from '../src/IMAGES/twg.JPG'
-import s200 from '../src/IMAGES/s200sx.JPG'
+import xsp from '../src/IMAGES/xsp.JPG'
+import iplim from '../src/IMAGES/iplim.JPG'
 
 
 
@@ -36,23 +37,42 @@ class getDetailTextForBoost extends Component {
       <div>
           <center>
           <h2>
-         <a href='https://www.trionictuning.com/forum/viewtopic.php?f=57&t=3474'>   <b>Boost</b> </a>   
-            </h2>
+            <a href='https://www.trionictuning.com/forum/viewtopic.php?f=57&t=3474'>   <b>Boost</b> </a>   
+          </h2>
      
             <img className="" src={pwm}   style={{maxWidth:'30%',height:'auto'}}alt="Logo" />
+
             <h4> Tuning your Boost/ Tuning Foreign Turbo Setups  </h4>
-            <h6>  Preliminaries : </h6>
+            <table>
+            <h4>  Preliminaries : </h4>
               <li> <b> Choosing Wastegate spring lb  </b> : Choose a spring thats somewhere 1/3- 1/2 of your desired boost level</li>
               <li> <b> too high Wastegate spring lb  </b> : wont allow us to have proper boost regulation</li>
               <li> <b> too low Wastegate spring lb  </b> : wont allow you to hit high enough targets </li>
+              <li> understand limiters and how they work together</li>
+              </table>
               
-            <b>Step 1 : Finding out when to acitvate the boost controller </b>
+            <b><h3>Step 1 : Finding out when to activate the boost controller</h3> </b>
             <p>The idea here is that we need to activate the boost controller when the turbo isnt yet fully spooled (when the base boost is hit) </p>
             
-            <b><p>So what we need to do is the following </p></b>
+            <b><p>So now lets disable the boost controller and log some full throttle runs </p></b>
+            <table>
                     <li> <b> turn off boost controller  </b> : boostcal.st_enable = 0 </li>
                     <li> <b> turn off boost adaptation </b> :  boostcal.EnableBoostadap = 0</li>
-                   <h5>Finish this tomorrow</h5>
+                   
+            <p>Once you complete this look for a good third gear pull throughout the rev range in your logs and fill in the following tables </p>
+                    <li> <b> BoostCal.P_LimTab </b> </li>
+                    <li> <b> BoostCal.I_LimTab </b> </li>
+                    <p>So once the mair goes higher then the values in these tables then your boost controller will kick in</p>
+            </table>
+                    <img className="" src={iplim}   style={{maxWidth:'30%',height:'auto'}}alt="Logo" />
+             <b><h3>Step 2  : Setting your boost intervals/tuning the boostcal.regmap</h3> </b>
+                    <p>So since we have a higher base boost than before we now have to rescale our boostcal.regmap x-axis parameters</p>
+                    <b><p>BoostCal.SetLoadXSP</p></b>
+                    <img className="" src={xsp}   style={{maxWidth:'30%',height:'auto'}}alt="Logo" />
+                    <p>So in my logs I hit a base boost of about 1200 so this is a good starting point.
+                    I also added about 100 to every column until it was filled</p>
+                    <b>Note: based on the turbo setup you may have more flowing capabilities or less so determining how high you can go is based on the flow characteristics of your turbo</b>
+
 
 
        
--- src/getDetailTextForLimiters.jsx
+++ src/getDetailTextForLimiters.jsx
@@ -30,29 +30,44 @@ class GetDetailTextForLimiters extends Component {
 
           
                 </div>
-                <h4>Description : </h4>
-                <h6> Various maps that limit your engines perfromance </h6>
-               
-                <h6> Preliminaries : 
-                    <p> <b> Petrol torque</b> : (Airmass /3.1) = estimated petrol torque(Nm)</p>
-                    <p> <b> E85 torque</b> : (Airmass / 2.9) = estimated e85 torque(Nm)</p>
-                 </h6>
-                <h4><b> Common Limiting Maps </b></h4>
-                <div>
-                    <li> <b> torqueCal.m_AirTorqueMap</b> : gearbox max torque limiter, the 350/370 column has to be altered(fooled) to achieve higher tq.</li>
-                    <li> <b> torqueCal.M_mangearLim </b> : max allowed torque limit per gear</li>
-                    <li> <b> torqueCal.M_EngMaxTab </b> : max engine output tq </li>
-                    <li> <b> bstKnkCal.MaxAirmass</b> : Max requested airmass , most right column is used if no knock is detected</li>
-                    <p> <b>Others under the limiters tab in t7 suite</b> </p>
-                </div>
+                <table>
+                  <tr>
+                    <b><h4>Description : </h4></b>
+                    
 
+                    Various maps that limit your engines perfromance 
+                    
+                  </tr>
+               <tr>
+                <h4> Preliminaries : </h4>
+                <p>
+                  <li> <b>Petrol torque</b> : (Airmass /3.1) = estimated petrol torque(Nm)</li>
+                  <li><b> E85 torque</b> : (Airmass / 2.9) = estimated e85 torque(Nm)</li>
+                  </p>
+                 </tr>
+                </table>
+                <h4><b> Common Limiting Maps </b></h4>
+                
+                <table>
+                <div>
+                <h10>
+                    <p> <b> torqueCal.m_AirTorqueMap</b> : gearbox max torque pmiter, the 350/370 column has to be altered(fooled) to achieve higher tq.</p>
+                    <p> <b> torqueCal.M_mangearpm </b> : max allowed torque pmit per gear</p>
+                    <p> <b> torqueCal.M_EngMaxTab </b> : max engine output tq </p>
+                    <p> <b> bstKnkCal.MaxAirmass</b> : Max requested airmass , most right column is used if no knock is detected</p>
+                    <p> <b>Others under the limiters tab in t7 suite</b> </p>
+                    </h10>
+                </div>
+                </table>
+                <h4><b> Important  </b></h4>
                 <h7><b>When altering these maps I tend to constantly check my airmass result viewer to make sure there are no limiting factors prior to flashing my ecu</b></h7>
                     <p> <b>Actions -> Airmass result viewer</b> </p>
-                <h4>So you changed all your airmass and torque limiters and you go to the airmass result view and see you have a bunch of limiters and the airmass values have not increased</h4>
-                <h4>This is because the limiters need to be altered in order to achieve your desired output </h4>
-                <img className="" src={arv}   style={{maxWidth:'50%',height:'auto'}}alt="Logo" />
+                    <h4><b> Example Situation  </b></h4>
+                <h8>So you changed all your airmass and torque limiters and you go to the airmass result view and see you have a bunch of limiters and the airmass values have not increased</h8>
+                <h8>This is because the limiters need to be altered in order to achieve your desired output </h8>
 
                   </center>
+                <img className="" src={arv}   style={{maxWidth:'50%',height:'auto'}}alt="Logo" />
                   <div>
                 <b>Ex: Here i had to change my turbospeedlimiter (raised to 1.0 across the board ) to achieve my 1700 mair request </b>
                 </div>
